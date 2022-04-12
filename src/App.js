22f3c082d457ad2a0f330403105decee0309c4bf
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-04-12 23:41:08 +0800

    [修改] 載入全局 sass

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App main-blue">
+      <header className="App-header main-blue">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style/all.sass'
 
 ReactDOM.render(
   <React.StrictMode>
