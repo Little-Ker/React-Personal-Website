cba42af4d599f4c3dbe44dea0f4257f3a616f299
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-04-17 15:35:49 +0800

    [新增] redux 環境

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './style/all.sass'
 
+import { Provider } from "react-redux";
+import { store } from './store'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
